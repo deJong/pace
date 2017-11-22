efa25499dc446f5520499badd8501046e8ed29dc
Stijn de Jong <stijn@stijndejong.com>
2017-11-22 17:15:40 +0100

    Remove % from data-text

--- pace.js
+++ pace.js
@@ -294,7 +294,7 @@
         el.children[0].style[key] = transform;
       }
       if (!this.lastRenderedProgress || this.lastRenderedProgress | 0 !== this.progress | 0) {
-        el.children[0].setAttribute('data-progress-text', "" + (this.progress | 0) + "%");
+        el.children[0].setAttribute('data-progress-text', "" + (this.progress | 0));
         if (this.progress >= 100) {
           progressStr = '99';
         } else {
